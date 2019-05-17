30222a4347a9650639e4132f0662a9aa1f60d8d5
Sanchit Samuel <sanchit.samuel@live.com>
2019-05-17 15:03:18 +0530

    *Topic optimized

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,5 @@ class Question < ApplicationRecord
   self.inheritance_column = "QWERTY"
   has_one :detail, class_name: "QuestionDetail", foreign_key: "questionId"
   # has_many :topics, through: :topic_questions
-  has_and_belongs_to_many :topics, join_table: "topic_questions", foreign_key: "topic_id"
+  has_and_belongs_to_many :topics, join_table: "topic_questions", foreign_key: "question_id"
 end
