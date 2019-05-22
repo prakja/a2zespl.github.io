b527bc13b32070fc71c9c7d3020db8bbfb4ea49c
jay <jay.prakash1@gmail.com>
2019-05-22 18:14:09 +0530

    changes to add sub topic

--- app/models/sub_topic.rb
+++ app/models/sub_topic.rb
@@ -2,5 +2,8 @@ class SubTopic < ApplicationRecord
   self.table_name = "SubTopic"
   belongs_to :topic, class_name: "Topic", foreign_key: "topicId"
   # has_many :subTopicQuestions, {where(assetType: 'Question', deleted: false)}, foreign_key: :ownerId, class_name: 'TopicAsset'
+  attribute :createdAt, :datetime, default: Time.now
+  attribute :updatedAt, :datetime, default: Time.now
+  attribute :deleted, :boolean, default: false
 
 end
