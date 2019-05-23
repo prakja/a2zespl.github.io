19a6a093a7550a0f204a62045cf5b343c3a458b9
jay <jay.prakash1@gmail.com>
2019-05-23 10:52:57 +0530

    only get topics for pcb for now, don't get for LR and aptitude

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ApplicationRecord
   self.table_name = "Topic"
-  scope :neetprep_course, -> {joins(:subject).where(Subject: {courseId:  8})}
+  # only include pcb topics for now
+  scope :neetprep_course, -> {joins(:subject).where(Subject: {courseId:  8, id: [53,54,55,56]})}
   has_many :topicQuestions, -> {where(assetType: 'Question', deleted: false)}, foreign_key: :topicId, class_name: 'TopicAsset'
   has_many :questions, through: :topicQuestions
   belongs_to :subject, foreign_key: 'subjectId', class_name: 'Subject'
