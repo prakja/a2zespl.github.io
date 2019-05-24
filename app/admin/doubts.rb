0e9df69ca4548e0c3c59d118e0c18b78e0f5fbe2
jay <jay.prakash1@gmail.com>
2019-05-24 16:50:57 +0530

    allow params which will be edited by teachers

--- app/admin/doubts.rb
+++ app/admin/doubts.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Doubt do
+  permit_params :content, :deleted, :teacherReply, :imgUrl
   scope :botany_paid_student_doubts
   scope :chemistry_paid_student_doubts
   scope :physics_paid_student_doubts
