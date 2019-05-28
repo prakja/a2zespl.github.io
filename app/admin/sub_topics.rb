b2bcc000b0bec31312c53b87771a74e7f009c6b5
Sanchit Samuel <sanchit.samuel@live.com>
2019-05-28 17:40:04 +0530

    *Chapter filter in sub topic

--- app/admin/sub_topics.rb
+++ app/admin/sub_topics.rb
@@ -11,8 +11,11 @@ ActiveAdmin.register SubTopic do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-remove_filter :questions, :subTopicQuestions, :topic, :subTopicVideos, :videos
+  remove_filter :questions, :subTopicQuestions, :topic, :subTopicVideos, :videos
   permit_params :name, :topicId
+
+  filter :topic_id_eq, as: :select, collection: -> { Topic.name_with_subject }, label: "Chapter"
+  preserve_default_filters!
 
   form do |f|
     f.inputs "Sub Topic" do
