366691ea93e4a2cb7c589732570e889cb2d021f4
jay <jay.prakash1@gmail.com>
2019-06-01 17:42:53 +0530

    fix chapter note model

--- app/models/chapter_note.rb
+++ app/models/chapter_note.rb
@@ -1,5 +1,5 @@
 class ChapterNote < ApplicationRecord
   self.table_name = "ChapterNote"
   belongs_to :note, foreign_key: 'noteId'
-  belongs_to :topic, foreign_key: 'chapterId', :class_name: 'Topic'
+  belongs_to :topic, foreign_key: 'chapterId', class_name: 'Topic'
 end
