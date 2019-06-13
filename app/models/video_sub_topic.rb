876757956779a053b2b97470695dc86010439ad2
tech 1 <shivam@goodeducator.com>
2019-06-13 18:52:19 +0530

    added optional: true

--- app/models/video_sub_topic.rb
+++ app/models/video_sub_topic.rb
@@ -1,7 +1,7 @@
 class VideoSubTopic < ApplicationRecord
   self.table_name = "VideoSubTopic"
-  belongs_to :video, foreign_key: 'videoId'
-  belongs_to :subTopic, foreign_key: 'subTopicId', class_name: 'SubTopic'
+  belongs_to :video, foreign_key: 'videoId', optional: true
+  belongs_to :subTopic, foreign_key: 'subTopicId', class_name: 'SubTopic', optional: true
   attribute :createdAt, :datetime, default: Time.now
   attribute :updatedAt, :datetime, default: Time.now
 end
