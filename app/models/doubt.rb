9f1837b721399810090bdefca6c024f1c055e6be
jay <jay.prakash1@gmail.com>
2019-06-13 20:02:03 +0530

    add filters on student name, email and phone

--- app/models/doubt.rb
+++ app/models/doubt.rb
@@ -1,9 +1,19 @@
 class Doubt < ApplicationRecord
   self.table_name = "Doubt"
   belongs_to :topic, class_name: "Topic", foreign_key: "topicId"
+  belongs_to :user, class_name: "User", foreign_key: "userId"
   has_many :answers, foreign_key: "doubtId", class_name: "DoubtAnswer"
-  scope :subject_doubts, ->(subject_id) {
+  scope :subject_name, ->(subject_id) {
     joins(:topic => :subjects).where(topic: {Subject: {id: subject_id}})
+  }
+  scope :student_name, ->(name) {
+    joins(:user => :user_profile).where('"UserProfile"."displayName" ILIKE ?', "%#{name}%")
+  }
+  scope :student_email, ->(email) {
+    joins(:user => :user_profile).where('"UserProfile"."email" ILIKE ? or "User"."email" ILIKE ?', "%#{email}%", "%#{email}%")
+  }
+  scope :student_phone, ->(phone) {
+    joins(:user => :user_profile).where('"UserProfile"."phone" ILIKE ? or "User"."phone" ILIKE ?', "%#{phone}%", "%#{phone}%")
   }
   scope :solved, ->(solved) {
     if solved == "yes"
@@ -20,13 +30,13 @@ class Doubt < ApplicationRecord
       where.not(UserCourse.where('"UserCourse"."userId" = "Doubt"."userId" AND "expiryAt" >= CURRENT_TIMESTAMP').exists)
     end
   }
-  scope :botany_paid_student_doubts, -> {solved('no').paid('yes').subject_doubts(53)}
-  scope :chemistry_paid_student_doubts, -> {solved('no').paid('yes').subject_doubts(54)}
-  scope :physics_paid_student_doubts, -> {solved('no').paid('yes').subject_doubts(55)}
-  scope :zoology_paid_student_doubts, -> {solved('no').paid('yes').subject_doubts(56)}
+  scope :botany_paid_student_doubts, -> {solved('no').paid('yes').subject_name(53)}
+  scope :chemistry_paid_student_doubts, -> {solved('no').paid('yes').subject_name(54)}
+  scope :physics_paid_student_doubts, -> {solved('no').paid('yes').subject_name(55)}
+  scope :zoology_paid_student_doubts, -> {solved('no').paid('yes').subject_name(56)}
   
   def self.ransackable_scopes(_auth_object = nil)
-    [:subject_doubts, :solved, :paid]
+    [:subject_name, :solved, :paid, :student_name, :student_email, :student_phone]
   end
 
 end
