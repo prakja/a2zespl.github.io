1fd755df117946b533f29f457302066ca2b26d84
jay <jay.prakash1@gmail.com>
2019-06-19 12:20:44 +0530

    fix courses list ordering in course invitation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
   self.table_name = "Course"
   self.inheritance_column = "QWERTY"
-  scope :public_courses, -> {where(package: 'neet')}
+  scope :public_courses, -> {where(package: 'neet').order('"id" ASC')}
   has_many :payments, class_name: "Payment", foreign_key: "paymentForId"
   has_many :courseInvitations, class_name: "CourseInvitation", foreign_key: "courseId"
 
