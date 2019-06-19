49aa180cbf58d385342b676cc32def6cc21213c6
jay <jay.prakash1@gmail.com>
2019-06-19 11:59:15 +0530

    fix courses list in course invitation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
   self.table_name = "Course"
   self.inheritance_column = "QWERTY"
-  scope :public_courses, -> {where(public: true)}
+  scope :public_courses, -> {where(package: 'neet')}
   has_many :payments, class_name: "Payment", foreign_key: "paymentForId"
   has_many :courseInvitations, class_name: "CourseInvitation", foreign_key: "courseId"
 
