30e6edf335da895f1d16f2669d7f054335bda252
Sanchit Samuel <sanchit.samuel@live.com>
2019-06-21 20:29:42 +0530

    *support model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
  has_many :schedule_item_users, class_name: "ScheduleItemUser", foreign_key: "userId"
  has_one :user_profile, class_name: "UserProfile", foreign_key: "userId"
+ has_many :customer_supports, class_name: "CustomerSupport", foreign_key: "userId"
 
  def name
   if not self.user_profile.blank? and not self.user_profile.displayName.blank?
