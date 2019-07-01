22c30175ddf209c20eaba9797c1d2bb48fda8ed3
shivamgooded <shivam@goodeducator.com>
2019-07-01 13:13:10 +0530

    before create validation and before update hooks course invitation

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -3,8 +3,7 @@ class Delivery < ApplicationRecord
   attribute :createdAt, :datetime, default: Time.now
   attribute :updatedAt, :datetime, default: Time.now
   has_paper_trail
-  validate :check_installment_required_dueAmount
-  validate :check_installment_required_dueDate
+  after_validation :check_installment_required_dueAmount, :check_installment_required_dueDate
   validates_presence_of :deliveryType, :course, :courseValidity, :purchasedAt, :name, :email, :mobile, :address, :counselorName
 
   def check_installment_required_dueAmount
