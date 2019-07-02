d37f5f6d2ab69843f04dac541e91aa53ded46ed2
shivamgooded <shivam@goodeducator.com>
2019-07-02 17:13:52 +0530

    remove course invitation filer from user course

--- app/admin/user_courses.rb
+++ app/admin/user_courses.rb
@@ -1,12 +1,13 @@
 ActiveAdmin.register UserCourse do
-  permit_params :course, :userId, :courseInvitation, :role, :role, :startedAt, :expiryAt, :courseInvitationId, :courseId
+  permit_params :course, :userId, :role, :role, :startedAt, :expiryAt, :invitationId, :courseId
   remove_filter :course
+  preserve_default_filters!
 
   index do
     id_column
     column :course
     column (:userId) { |userCourse| raw(userCourse.userId)  }
-    column :courseInvitation
+    column :invitationId
     column (:role) { |userCourse| raw(userCourse.role)  }
     column (:startedAt) { |userCourse| raw(userCourse.startedAt)  }
     column (:expiryAt) { |userCourse| raw(userCourse.expiryAt)  }
@@ -19,7 +20,7 @@ ActiveAdmin.register UserCourse do
     f.inputs "UserCourse" do
       f.input :course, as: :select, :collection => Course.public_courses
       f.input :userId, label: "User Id"
-      f.input :courseInvitation, label: "Course Invitation", as: :select, :collection => CourseInvitation.recent_course_invitations
+      f.input :invitationId, label: "Course Invitation"
       f.input :role, as: :select, :collection => ["courseStudent", "courseManager", "courseCreator", "courseAdmin"]
       f.input :startedAt, as: :date_picker, label: "Course start date"
       f.input :expiryAt, as: :date_picker, label: "Course expiry date"
--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -1,5 +1,7 @@
 class UserCourse < ApplicationRecord
  self.table_name = "UserCourse"
  belongs_to :course, foreign_key: "courseId", class_name: "Course", optional: false
- belongs_to :courseInvitation, foreign_key: "invitationId", class_name: "CourseInvitation", optional: true
+ # belongs_to :courseInvitation, foreign_key: "invitationId", class_name: "CourseInvitation", optional: true
+ attribute :createdAt, :datetime, default: Time.now
+ attribute :updatedAt, :datetime, default: Time.now
 end
