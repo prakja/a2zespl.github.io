bcd4d01a334ea7c15283a00198dbf8170bac2612
Sanchit Samuel <sanchit.samuel@live.com>
2019-07-15 17:46:58 +0530

    *added loggin to user course

--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -1,5 +1,6 @@
 class UserCourse < ApplicationRecord
  self.table_name = "UserCourse"
+ has_paper_trail
  belongs_to :course, foreign_key: "courseId", class_name: "Course", optional: false
  # belongs_to :courseInvitation, foreign_key: "invitationId", class_name: "CourseInvitation", optional: true
  attribute :createdAt, :datetime, default: Time.now
