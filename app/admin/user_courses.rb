556f1ec3d98715982681a4f88d33c0d192f546fe
shivamgooded <shivam@goodeducator.com>
2019-07-16 17:46:42 +0530

    some changes in user course

--- app/admin/user_courses.rb
+++ app/admin/user_courses.rb
@@ -1,18 +1,41 @@
 ActiveAdmin.register UserCourse do
-  permit_params :course, :userId, :role, :role, :startedAt, :expiryAt, :invitationId, :courseId
-  remove_filter :course, :versions
+  permit_params :course, :userId, :role, :startedAt, :expiryAt, :invitationId, :courseId
+  remove_filter :course, :versions, :invitation
   preserve_default_filters!
+
+  filter :invitationId_eq, as: :number, label: "Invitation ID"
 
   index do
     id_column
     column :course
     column (:userId) { |userCourse| raw(userCourse.userId)  }
     column :invitationId
+    column ("email") { |userCourse|
+      if userCourse.invitation
+        userCourse.invitation.email
+      end
+    }
     column (:role) { |userCourse| raw(userCourse.role)  }
     column :startedAt
     column :expiryAt
     column :createdAt
     actions
+  end
+
+  csv do
+    column ("course") { |userCourse|
+      if userCourse.course
+        userCourse.course.name
+      end
+    }
+    column ("email") { |userCourse|
+      if userCourse.invitation
+        userCourse.invitation.email
+      end
+    }
+    column :startedAt
+    column :expiryAt
+    column :createdAt
   end
 
   form do |f|
