498c92d217546cbc83b98d960a39f62e90a2ab5b
Sanchit Samuel <sanchit.samuel@live.com>
2019-07-16 17:04:19 +0530

    *Notification fix *open answer page in new tab

--- app/admin/notifications.rb
+++ app/admin/notifications.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register Notification do
   remove_filter :user
   filter :userId_eq, as: :number, label: "User ID"
+  preserve_default_filters!
+
+  index do
+
+    id_column
+    column :title
+    column :user
+    column (:body) { |notification| raw(notification.body)  }
+    actions
+  end
 end
