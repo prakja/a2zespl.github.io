003e95992b35d0a37773d12eab8cb0c23c707edf
shivamgooded <shivam@goodeducator.com>
2019-07-19 12:07:50 +0530

    course creation from admin

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,19 @@
 class Course < ApplicationRecord
   self.table_name = "Course"
   self.inheritance_column = "QWERTY"
+
+  before_create :setCreatedTime, :setUpdatedTime
+  before_update :setUpdatedTime
+  has_paper_trail
+
+  def setCreatedTime
+    self.createdAt = Time.now
+  end
+
+  def setUpdatedTime
+    self.updatedAt = Time.now
+  end
+
   scope :public_courses, -> {where(package: 'neet').order('"id" ASC')}
   has_many :payments, class_name: "Payment", foreign_key: "paymentForId"
   has_many :courseInvitations, class_name: "CourseInvitation", foreign_key: "courseId"
