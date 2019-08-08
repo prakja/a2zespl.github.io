83f583edbb7046f1adbf0def23ca00af75d7cf49
shivamgooded <shivam@goodeducator.com>
2019-08-08 14:28:34 +0530

    show physics,chemistry,biology score on test attempt

--- app/admin/test_attempts.rb
+++ app/admin/test_attempts.rb
@@ -1,15 +1,40 @@
 ActiveAdmin.register TestAttempt do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  index do
+    id_column
+    column :userId
+    column :testId
+    column :elapsedDurationInSec
+    column :currentQuestionOffset
+    column :completed
+    column :userAnswers
+    column :userQuestionWiseDurationInSec
+    column :result
+    column :createdAt
+    column :updatedAt
+    column :visitedQuestions
+    column :markedQuestions
+    column "Physics Score" do |testAttempt|
+      if testAttempt.result
+        if testAttempt.result['sections']
+          testAttempt.result['sections'][2]['totalMarks']
+        end
+      end
+    end
+    column "Chemistry Score" do |testAttempt|
+      if testAttempt.result
+        if testAttempt.result['sections']
+          testAttempt.result['sections'][1]['totalMarks']
+        end
+      end
+    end
+    column "Biology Score" do |testAttempt|
+      if testAttempt.result
+        if testAttempt.result['sections']
+          testAttempt.result['sections'][0]['totalMarks']
+        end
+      end
+    end
+    actions
+  end
 
 end
