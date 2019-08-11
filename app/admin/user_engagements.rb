2d1e2f1487db79055f24de7e6ff49d321c38eb6e
jay <jay.prakash1@gmail.com>
2019-08-11 14:20:44 +0530

    changes to separate graphs

--- app/admin/user_engagements.rb
+++ app/admin/user_engagements.rb
@@ -1,5 +1,16 @@
 ActiveAdmin.register_page "User Engagements" do
   content do
     render partial: 'graph'
+    tabs do
+      tab :active_users do
+        render partial: 'active_users'
+      end
+      tab :solved_questions do
+        render partial: 'solved_questions'
+      end
+      tab :user_courses do
+        render partial: 'user_courses'
+      end
+    end
   end
 end
