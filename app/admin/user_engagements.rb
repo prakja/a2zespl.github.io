2d1a3eb37e23d52ebf0b555e7050760a9a649431
jay <jay.prakash1@gmail.com>
2019-08-11 21:28:10 +0530

    ajax loading of graphs

--- app/admin/user_engagements.rb
+++ app/admin/user_engagements.rb
@@ -1,9 +1,22 @@
 ActiveAdmin.register_page "User Engagements" do
+
+  page_action :new_users_json, method: :get do
+    render json: User.group_by_day(:createdAt, range: 1.months.ago.midnight..Time.now).count
+  end
+
+  page_action :solved_questions_json, method: :get do
+    render json: Answer.group_by_day(:createdAt, range: 1.months.ago.midnight..Time.now).count
+  end
+
+  page_action :user_courses_json, method: :get do
+    render json: UserCourse.group_by_day(:createdAt, range: 1.months.ago.midnight..Time.now).count
+  end
+
   content do
     render partial: 'graph'
     tabs do
-      tab :active_users do
-        render partial: 'active_users'
+      tab :new_users do
+        render partial: 'new_users'
       end
       tab :solved_questions do
         render partial: 'solved_questions'
