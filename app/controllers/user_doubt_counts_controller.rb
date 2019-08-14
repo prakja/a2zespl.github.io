c9fe653d06d33f28f7eee228088830a861d60e01
Sanchit Samuel <sanchit.samuel@live.com>
2019-08-14 17:06:59 +0530

    *User stats

--- app/controllers/user_doubt_counts_controller.rb
+++ app/controllers/user_doubt_counts_controller.rb
@@ -3,8 +3,17 @@ class UserDoubtCountsController < ApplicationController
 
   # GET /user_doubt_counts
   # GET /user_doubt_counts.json
-  def index
+  def stats
     userId=[params[:user]][0]
+    startDate = [params[:startDate]][0]
+    endDate = [params[:endDate]][0]
+
+    if startDate.blank? && endDate.blank?
+      endDate = Date.current
+      startDate = endDate.last_month
+      redirect_to "/user_doubt_counts/stats?user=" + userId.to_s + "&startDate=" + startDate.year.to_s + "-" + startDate.month.to_s.rjust(2, '0') + "-" + startDate.day.to_s.rjust(2, '0') + "&endDate=" + endDate.year.to_s + "-" + endDate.month.to_s.rjust(2, '0') + "-" + endDate.day.to_s.rjust(2, '0')
+    end
+
     begin
       @user = User.find(userId)
     rescue ActiveRecord::RecordNotFound
@@ -16,6 +25,16 @@ class UserDoubtCountsController < ApplicationController
       doubts = Doubt.joins(:topic => :subjects).where(userId: @user.id, deleted: [true, false], topic: {Subject: {id: subject.id}})
       if not doubts.blank?
         @doubts_subject[subject] = doubts
+      end
+    end
+
+    @doubts_subject_time = Hash.new
+    Subject.where(id: [53, 54, 55, 56]).each do |subject|
+      doubts = Doubt.joins(:topic => :subjects)
+                .where(userId: @user.id, deleted: [true, false], topic: {Subject: {id: subject.id}})
+                .where('"Doubt"."createdAt" > ? AND "Doubt"."createdAt" < ?', startDate, endDate)
+      if not doubts.blank?
+        @doubts_subject_time[subject] = doubts
       end
     end
   end  
