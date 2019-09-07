d87928874c2f44ae893ec962b5fd54abf453384b
jay <jay.prakash1@gmail.com>
2019-09-07 15:33:36 +0530

    test and user filter in test leaderboard

--- app/admin/test_leader_boards.rb
+++ app/admin/test_leader_boards.rb
@@ -1,4 +1,7 @@
 ActiveAdmin.register TestLeaderBoard do
   remove_filter :test, :user, :test_attempt
+  preserve_default_filters!
   scope :paid_students
+  filter :testId_eq, as: :number, label: "Test ID"
+  filter :userId_eq, as: :number, label: "User ID"
 end
