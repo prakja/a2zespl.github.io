09f2b7e9d4716fcceadd62f97489ad6299be1989
jay <jay.prakash1@gmail.com>
2019-09-08 15:09:35 +0530

    some changes for answer to show correct and incorrect

--- app/admin/answers.rb
+++ app/admin/answers.rb
@@ -4,4 +4,15 @@ ActiveAdmin.register Answer do
   filter :userId_eq, as: :number, label: "User ID"
   filter :questionId_eq, as: :number, label: "Question ID"
   filter :testAttemptId_eq, as: :number, label: "TestAttempt ID"
+
+  scope :correct_answers
+  scope :incorrect_answers
+
+  index do
+    id_column
+    column (:question) {|answer| raw(answer.question.question)}
+    column (:correct) {|answer| answer.correct ? 'yes' : 'no'}
+    column (:userAnswer) {|answer| answer.question.options[answer.userAnswer]}
+    column (:correctOption) {|answer| answer.question.options[answer.question.correctOptionIndex]}
+  end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,6 +4,18 @@ class Answer < ApplicationRecord
   belongs_to :question, foreign_key: "questionId", class_name: "Question"
   belongs_to :testAttempt, foreign_key: "testAttemptId", class_name: "TestAttempt", optional: true
 
+  def correct
+    return self.userAnswer == self.question.correctOptionIndex
+  end
+
+  scope :correct_answers, -> {
+    joins(:question).where('"Question"."correctOptionIndex" = "userAnswer"')
+  }
+
+  scope :incorrect_answers, -> {
+    joins(:question).where('"Question"."correctOptionIndex" != "userAnswer"')
+  }
+
   scope :paid, ->(paid, start_date, end_date) {
     if paid == "yes"
       where(UserCourse.where('"UserCourse"."userId" = "Answer"."userId" AND "UserCourse"."createdAt" <= ? and "UserCourse"."createdAt" >= ?', "#{start_date}", "#{end_date}").exists)
