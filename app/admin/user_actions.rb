d880723116ff07e23a38b02433ff826de6191c79
shivamgooded <shivam@goodeducator.com>
2019-09-16 12:30:56 +0530

    free users scope in user actions

--- app/admin/user_actions.rb
+++ app/admin/user_actions.rb
@@ -1,26 +1,15 @@
 ActiveAdmin.register UserAction do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-remove_filter :user
+  remove_filter :user
+  scope :free_users
 
-index do
-  id_column
-  column :user
-  column :count
-  actions
-end
+  index do
+    id_column
+    column :user
+    column :count
+    actions
+  end
 
-filter :user_id_eq, as: :number, label: "User ID"
-preserve_default_filters!
+  filter :user_id_eq, as: :number, label: "User ID"
+  preserve_default_filters!
 
 end
