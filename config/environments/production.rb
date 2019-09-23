e14790edd4146ba23e027346fee5a2f17c3b00b7
Sanchit Samuel <sanchit.samuel@live.com>
2019-09-23 19:08:40 +0530

    *enabled ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  config.force_ssl = true
+  config.ssl_options = {  redirect: { status: 307, port: 81 } }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
