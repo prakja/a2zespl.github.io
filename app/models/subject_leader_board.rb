d98b7ee70361d95c88eb1df59b452d51a798f038
jay <jay.prakash1@gmail.com>
2019-10-01 20:09:33 +0530

    Added subject and common leader board

---
+++ app/models/subject_leader_board.rb
@@ -0,0 +1,7 @@
+class SubjectLeaderBoard < ApplicationRecord
+  self.table_name = "SubjectLeaderBoard"
+  self.primary_key = "id"
+  belongs_to :subject, class_name: "Subject", foreign_key: "subjectId"
+  belongs_to :user, class_name: "User", foreign_key: "userId"
+  scope :paid_students, -> {where(UserCourse.where('"UserCourse"."userId" = "SubjectLeaderBoard"."userId"').limit(1).arel.exists)}
+end
