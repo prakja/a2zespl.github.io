ebf190b5f8d06b19ef70016e423134819a8e7c2b
shivamgooded <shivam@goodeducator.com>
2019-10-16 11:22:38 +0530

    show video watched count on user activity

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -46,7 +46,6 @@ index do
   column :email
   column :phone
   column :role
-  column :video_count, sortable: true
   actions
 end
 
@@ -60,6 +59,7 @@ sidebar :user_activity, only: :show do
     li link_to "Subject Ranks", admin_subject_leader_boards_path(q: {userId_eq: user.id}, order: 'rank_asc')
     li link_to "Topic Ranks", admin_topic_leader_boards_path(q: {userId_eq: user.id}, order: 'rank_asc')
     li link_to "Overall Rank", admin_common_leader_boards_path(q: {userId_eq: user.id}, order: 'rank_asc')
+    li raw("Videos Watched " + "(" + user.user_video_stats.count.to_s + ")")
   end
 end
 
