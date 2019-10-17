04b8ba086032cc71e655eb853f68c288c6168dbe
Sanchit Samuel <sanchit.samuel@live.com>
2019-10-17 16:51:11 +0530

    *SchedeleItemAsset are not savable

--- app/admin/schedule_item_assets.rb
+++ app/admin/schedule_item_assets.rb
@@ -1,4 +1,15 @@
 ActiveAdmin.register ScheduleItemAsset do
   remove_filter :scheduleItem
-  permit_params :createdAt, :updatedAt, :asset, :scheduleItem, :ScheduleItem_id
+  permit_params :createdAt, :updatedAt, :assetName, :assetLink, :scheduleItem, :ScheduleItem_id
+
+  form do |f|
+    f.inputs "Schedule Item Asset" do
+      f.input :assetName, as: :string
+      f.input :assetLink, as: :string
+      f.input :scheduleItem, input_html: { class: "select2" }, :collection => ScheduleItem.topper_schedule_items
+      # f.input :createdAt, label: "Created At", as: :datetime_picker
+      # f.input :updatedAt, label: "Updated At", as: :datetime_picker
+    end
+    f.actions
+  end
 end
--- app/models/schedule_item.rb
+++ app/models/schedule_item.rb
@@ -8,4 +8,8 @@ class ScheduleItem < ApplicationRecord
   belongs_to :topic, class_name: "Topic", foreign_key: "topicId"
   has_many :scheduleItemUsers, class_name: "ScheduleItemUser", foreign_key: "scheduleItemId"
   has_many :scheduleItemAssets, class_name: "ScheduleItemAsset", foreign_key: "ScheduleItem_id"
+
+  def self.topper_schedule_items
+    ScheduleItem.where(scheduleId: 5).pluck("name", "id")
+  end
 end
