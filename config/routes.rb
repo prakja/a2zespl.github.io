475320caa6a1e60850443acf25a6363e79fa66ac
Sanchit Samuel <sanchit.samuel@live.com>
2019-10-18 16:31:47 +0530

    audio rec on questions

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   get "user_analytics/show"
   post "user_analytics/populate_user_activites"
   get "questions/pdf_questions"
+  get "questions/add_explanation/:id", to: "questions#add_explanation"
+  post "questions/update_explanation", to: "questions#update_explanation"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
