09cbb7afaf4465c9298f6645277836e01949e9e1
Sanchit Samuel <sanchit.samuel@live.com>
2019-10-21 17:37:25 +0530

    *proof read toggle

---
+++ db/migrate/20191021115016_add_question_proof_read_bool.rb
@@ -0,0 +1,6 @@
+class AddQuestionProofReadBool < ActiveRecord::Migration[5.2]
+  def change
+    add_column "Question", "proofRead", :boolean, default: false
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
