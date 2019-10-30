a97e173ce1a312ead513a5829e9185b7980d4f0f
deepakypro <deepak4u93@gmail.com>
2019-10-30 15:35:15 +0530

    added booster paid, unpaid user

--- app/controllers/course_details_controller.rb
+++ app/controllers/course_details_controller.rb
@@ -43,4 +43,26 @@ class CourseDetailsController < ApplicationController
       @course_subTopics_count += @course_topics_subTopics[topic.id]
     end
   end
+
+  def booster
+    if not current_admin_user
+      redirect_to "/admin/login"
+      return
+    end
+
+    @paidUser = params[:paidUser] && params[:paidUser] === "false" ? false : true
+
+    begin
+      @users = nil
+      if @paidUser === true  
+        @users = User.connection.select_all("SELECT \"User\".\"id\" AS \"userId\",\"User\".\"email\" AS \"email\",\"User\".\"phone\" AS \"phone\",\"UserProfile\".\"displayName\" AS \"displayName\",\"UserProfile\".\"phone\" AS \"uphone\",\"UserProfile\".\"email\" AS \"uemail\" FROM \"User\" LEFT JOIN \"UserProfile\" ON \"User\".\"id\" = \"UserProfile\".\"userId\" WHERE \"User\".\"id\"  IN (SELECT \"UserCourse\".\"userId\" FROM \"UserCourse\" WHERE \"UserCourse\".\"userId\" IN (SELECT \"UserVideoStat\".\"userId\" FROM \"Subject\", \"Course\", \"SubjectChapter\", \"ChapterVideo\", \"UserVideoStat\"  WHERE  \"UserVideoStat\".\"videoId\" = \"ChapterVideo\".\"videoId\" AND \"ChapterVideo\".\"chapterId\" = \"SubjectChapter\".\"chapterId\"  AND \"SubjectChapter\".\"subjectId\" = \"Subject\".\"id\" AND \"Subject\".\"courseId\" = \"Course\".\"id\" AND \"Course\".\"id\" = 149 AND  \"UserVideoStat\".\"userId\" NOT IN ( SELECT \"UserCourse\".\"userId\" FROM \"UserCourse\"  WHERE  \"UserCourse\".\"courseId\" = 8  AND \"UserCourse\".\"userId\" IN (SELECT \"UserVideoStat\".\"userId\" FROM \"Subject\", \"Course\", \"SubjectChapter\", \"ChapterVideo\", \"UserVideoStat\" WHERE  \"UserVideoStat\".\"videoId\" = \"ChapterVideo\".\"videoId\"  AND \"ChapterVideo\".\"chapterId\" = \"SubjectChapter\".\"chapterId\" AND \"SubjectChapter\".\"subjectId\" = \"Subject\".\"id\" AND \"Subject\".\"courseId\" = \"Course\".\"id\" AND \"Course\".\"id\" = 149 ) GROUP BY \"UserCourse\".\"userId\" HAVING COUNT (\"UserCourse\".\"userId\") > 0)) GROUP BY \"UserCourse\".\"userId\" HAVING COUNT (\"UserCourse\".\"userId\") > 0)");
+      else
+        @users = User.connection.select_all("SELECT \"User\".\"id\" AS \"userId\",\"User\".\"email\" AS \"email\",\"User\".\"phone\" AS \"phone\",\"UserProfile\".\"displayName\" AS \"displayName\",\"UserProfile\".\"phone\" AS \"uphone\",\"UserProfile\".\"email\" AS \"uemail\" FROM \"User\" LEFT JOIN \"UserProfile\" ON \"User\".\"id\" = \"UserProfile\".\"userId\" WHERE \"User\".\"id\" IN  (SELECT  \"UserVideoStat\".\"userId\" FROM \"Subject\",\"Course\",\"SubjectChapter\",\"ChapterVideo\",\"UserVideoStat\" WHERE  \"UserVideoStat\".\"videoId\" = \"ChapterVideo\".\"videoId\" AND \"ChapterVideo\".\"chapterId\" = \"SubjectChapter\".\"chapterId\" AND \"SubjectChapter\".\"subjectId\" = \"Subject\".\"id\" AND \"Subject\".\"courseId\" = \"Course\".\"id\" AND \"Course\".\"id\" = 149 AND  \"UserVideoStat\".\"userId\" NOT IN ( SELECT \"UserCourse\".\"userId\" FROM \"UserCourse\" WHERE \"UserCourse\".\"userId\" IN (SELECT  \"UserVideoStat\".\"userId\" FROM \"Subject\",\"Course\",\"SubjectChapter\",\"ChapterVideo\",\"UserVideoStat\" WHERE  \"UserVideoStat\".\"videoId\" = \"ChapterVideo\".\"videoId\" AND \"ChapterVideo\".\"chapterId\" = \"SubjectChapter\".\"chapterId\" AND \"SubjectChapter\".\"subjectId\" = \"Subject\".\"id\" AND \"Subject\".\"courseId\" = \"Course\".\"id\" AND \"Course\".\"id\" = 149) GROUP BY \"UserCourse\".\"userId\" HAVING COUNT (\"UserCourse\".\"userId\") > 0))")
+      end 
+      
+    rescue => exception
+      
+    end
+    
+  end
 end
