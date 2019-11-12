8e931e25629172b8e75c502ec02c6388eb0c75d0
shivamgooded <shivam@goodeducator.com>
2019-11-12 23:00:50 +0530

    load video player according to video on video links page

--- app/admin/video_links.rb
+++ app/admin/video_links.rb
@@ -22,18 +22,69 @@ ActiveAdmin.register VideoLink do
       f.input :name, as: :string
       f.input :url, as: :string
       panel 'Video' do
-        raw '<script src="https://unpkg.com/video.js/dist/video.js"></script>
-        <script src="https://unpkg.com/@videojs/http-streaming/dist/videojs-http-streaming.js"></script>
-        <link href="https://unpkg.com/video.js/dist/video-js.css" rel="stylesheet">
-        <div>
-          <video-js id="my_video_1" class="vjs-default-skin" controls preload="auto" width="640" height="268">
-            <source src="' + f.object.video.url + '" type="application/x-mpegURL">
-          </video-js>
-        </div>
-        <script>
-          player = videojs("my_video_1");
-          player.on("pause", function() {$("#video_link_time").val(player.currentTime())});
-        </script>'
+        if f.object.video.url.include? ".m3u8"
+          raw '<script src="https://unpkg.com/video.js/dist/video.js"></script>
+          <script src="https://unpkg.com/@videojs/http-streaming/dist/videojs-http-streaming.js"></script>
+          <link href="https://unpkg.com/video.js/dist/video-js.css" rel="stylesheet">
+          <div>
+            <video-js id="my_video_1" class="vjs-default-skin" controls preload="auto" width="640" height="268">
+              <source src="' + f.object.video.url + '" type="application/x-mpegURL">
+            </video-js>
+          </div>
+          <script>
+            player = videojs("my_video_1");
+            player.on("pause", function() {$("#video_link_time").val(player.currentTime())});
+          </script>'
+        elsif f.object.video.url.include? "youtube"
+          uri = URI.parse(f.object.video.url)
+          params = CGI.parse(uri.query)
+          raw '
+            <div id="player"></div>
+            <script>
+            var tag = document.createElement("script");
+
+            tag.src = "https://www.youtube.com/iframe_api";
+            var firstScriptTag = document.getElementsByTagName("script")[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+            var player;
+            function onYouTubeIframeAPIReady() {
+              player = new YT.Player("player", {
+                height: "390",
+                width: "640",
+                videoId: "' + params['v'].first  + '",
+                events: {
+                  "onStateChange": onPlayerStateChange
+                }
+              });
+            }
+
+            function onPlayerStateChange(event) {
+              if (event.data == YT.PlayerState.PAUSED) {
+                $("#video_link_time").val(player.getCurrentTime());
+              }
+            }
+          </script>
+          '
+        else
+          @urlArray = f.object.video.url.to_s.split('/')
+          @vimeoId = @urlArray[-1]
+          raw '<script src="https://player.vimeo.com/api/player.js"></script>
+          <div>
+            <iframe src="https://player.vimeo.com/video/'+@vimeoId+'" width="640" height="320" frameborder="0"  webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+          <script>
+              var iframe = document.querySelector("iframe");
+              var player = new Vimeo.Player(iframe);
+
+              player.on("pause", function() {
+                player.getCurrentTime().then(function(seconds) {
+                  $("#video_link_time").val(seconds);
+                });
+              });
+          </script>
+          '
+        end
       end
       f.input :time, hint: "To be entered in seconds. Ex: 493 would mean 8 minutes 13 seconds"
       f.input :videoId, label: "Video", as: :hidden, :input_html => { :value => f.object.videoId }
