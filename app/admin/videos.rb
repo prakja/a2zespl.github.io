366d79634fee72e46a9b98e737bfd7c110505c5c
shivamgooded <shivam@goodeducator.com>
2019-11-12 11:31:33 +0530

    view/ edit link for video panel items

--- app/admin/videos.rb
+++ app/admin/videos.rb
@@ -116,6 +116,7 @@ ActiveAdmin.register Video do
             seconds = ms / 1000
             raw(hours.to_s.rjust(2, '0') + ":" + minutes.to_s.rjust(2, '0') + ":" + seconds.to_s.rjust(2, '0'))
           end
+          column ("View / Edit") {|notes| raw('<a target="_blank" href="/admin/video_annotations/' + (notes.id).to_s + '">View / Edit</a>')}
         end
       end
       panel "Video Links" do
@@ -123,6 +124,7 @@ ActiveAdmin.register Video do
           column :name
           column :url
           column :time
+          column ("View / Edit") {|videoLinks| raw('<a target="_blank" href="/admin/video_links/' + (videoLinks.id).to_s + '">View / Edit</a>')}
         end
       end
     end
