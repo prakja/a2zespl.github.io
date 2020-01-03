124920d980c6625322e07d82033ecc2595f11993
shivamgooded <shivam@goodeducator.com>
2020-01-03 17:54:45 +0530

    some changes in campaign report

--- app/admin/campign_performance_reports.rb
+++ app/admin/campign_performance_reports.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register CampaignPerformanceReport do
   config.sort_order = 'day_desc'
+
+  index do
+    column :day
+    column :_sdc_sequence
+    column (:campaign) { |campaign_performance_report| raw(campaign_performance_report.campaign)  }
+    column (:cost) { |campaign_performance_report| raw(campaign_performance_report.cost/1000000).to_f  }
+    column (:avgCost) { |campaign_performance_report| raw(campaign_performance_report.avgCost/1000000).to_f  }
+    column (:campaignState) { |campaign_performance_report| raw(campaign_performance_report.campaignState)  }
+    column (:convRate) { |campaign_performance_report| raw(campaign_performance_report.convRate)  }
+    column (:conversions) { |campaign_performance_report| raw(campaign_performance_report.conversions)  }
+    column (:costConv) { |campaign_performance_report| raw(campaign_performance_report.costConv/1000000).to_f  }
+    column (:impressions) { |campaign_performance_report| raw(campaign_performance_report.impressions)  }
+    column (:interactions) { |campaign_performance_report| raw(campaign_performance_report.interactions)  }
+    column (:interactionRate) { |campaign_performance_report| raw(campaign_performance_report.interactionRate)  }
+    column :_sdc_customer_id
+    column :_sdc_extracted_at
+    actions
+  end
 end
