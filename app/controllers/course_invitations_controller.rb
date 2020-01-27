d7e33d2014d9fc86409b60796647d287902114e6
shivamgooded <shivam@goodeducator.com>
2020-01-27 11:59:08 +0530

    now we can send course invitation on multiple courses

---
+++ app/controllers/course_invitations_controller.rb
@@ -0,0 +1,57 @@
+class CourseInvitationsController < ApplicationController
+  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
+
+  def multiple_courses
+    if not current_admin_user
+      redirect_to "/admin/login"
+      return
+    end
+
+    @courses_data = {}
+    @current_admin_role = current_admin_user.role
+    @courses = Course.public_courses
+    @courses.each do |course|
+      @courses_data[course.id] = [course.name]
+    end
+  end
+
+  def createCourseInvitation
+    begin
+      @name = params[:name]
+      @email = params[:email]
+      @phone = params[:phone]
+      @courseIds = params[:courseIds]
+      @expiry = params[:expiry]
+
+      @rowsArray = []
+
+      @courseIds.each do |courseId|
+        @row = {}
+        @row["displayName"] = @name
+        @row["email"] = @email
+        @row["phone"] = @phone
+        @row["role"] = 'courseStudent'
+        @row["courseId"] = courseId.to_i
+        @row["expiryAt"] = @expiry
+        @row["skip_callback"] = true
+        @rowsArray.push(@row)
+      end
+
+      p @rowsArray
+
+      if(@courseIds.length() > 0)
+        CourseInvitation.create(@rowsArray)
+      end
+
+      respond_to do |format|
+        format.html { render :new }
+        format.json { render json: "Done", status: 200 }
+      end
+
+    rescue => exception
+
+    end
+  end
+
+end
