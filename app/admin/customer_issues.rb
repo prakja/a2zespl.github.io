203acf54da308d6716acabe0ece9481f06d39be2
Sanchit Samuel <sanchit.samuel@live.com>
2020-04-02 15:40:15 +0530

    *customer issue revamped on admin

--- app/admin/customer_issues.rb
+++ app/admin/customer_issues.rb
@@ -12,17 +12,36 @@ ActiveAdmin.register CustomerIssue do
 #   permitted
 # end
 
-remove_filter :topic, :question, :video
+remove_filter :topic, :question, :video, :user
 
 permit_params :resolved
 
 filter :topic_id_eq, as: :select, collection: -> { Topic.name_with_subject }, label: "Chapter"
+filter :user_id_eq, label: "User ID"
 preserve_default_filters!
+
+scope :botany_issues, show_count: true
+scope :chemistry_issues, show_count: true
+scope :physics_issues, show_count: true
+scope :zoology_issues, show_count: true
 
 form do |f|
   f.inputs "Issues" do
     f.input :resolved
   end
   f.actions
+end
+
+index do
+  id_column
+  column :description
+  column ("Type") {|issue| CustomerIssueType.find(issue.typeId).displayName}
+  column :question
+  column :video
+  column :note
+  column :topic
+  toggle_bool_column :resolved
+  column :user
+  actions
 end
 end
--- app/models/customer_issue.rb
+++ app/models/customer_issue.rb
@@ -4,4 +4,22 @@ class CustomerIssue < ApplicationRecord
  belongs_to :topic, foreign_key: :topicId, optional: true
  belongs_to :question, foreign_key: :questionId, optional: true
  belongs_to :video, foreign_key: :videoId, optional: true
+ belongs_to :user, foreign_key: :userId
+
+ scope :subject_name, ->(subject_id) {
+  joins(:topic => :subjects).where(topic: {Subject: {id: subject_id}})
+ }
+
+ scope :non_resolved, ->() {
+  where(resolved: false)
+ }
+
+ scope :botany_issues, -> {subject_name([53, 478, 132, 495, 390, 222]).non_resolved().distinct}
+ scope :chemistry_issues, -> {subject_name([54, 477, 129, 494, 391, 229, 169]).non_resolved().distinct}
+ scope :physics_issues, -> {subject_name([55, 476, 126, 493, 392, 232, 170]).non_resolved().distinct}
+ scope :zoology_issues, -> {subject_name([56, 479, 135, 496, 393, 234]).non_resolved().distinct}
+
+ def self.ransackable_scopes(_auth_object = nil)
+  [:subject_name]
+ end
 end
