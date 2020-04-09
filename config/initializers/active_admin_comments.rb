233f97ba6cf295bbb7351a1045e6f5359d452115
Sanchit Samuel <sanchit.samuel@live.com>
2020-04-09 14:26:23 +0530

    *added code to resolve support

---
+++ config/initializers/active_admin_comments.rb
@@ -0,0 +1,42 @@
+module ActiveAdmin
+  class Comment  < ActiveRecord::Base
+    after_create :send_notification
+
+    def send_notification
+      p "In notification block"
+      if self.resource_type == "CustomerSupport"
+        customer_support = CustomerSupport.find(self.resource_id)
+        user_id = customer_support.userId
+        user = User.find(user_id)
+        comment_body = self.body
+        p "Checking body and issue"
+        if not customer_support.resolved? and comment_body.start_with?("Resolved:")
+          customer_support.resolved = true
+          customer_support.save
+          HTTParty.post(
+            Rails.configuration.node_site_url + "api/v1/job/importantNewsNotification",
+            body: {
+              studentType: 'selected',
+              message: comment_body.gsub("Resolved:", "").squish,
+              title: "Issue Resolved",
+              imageUrl: nil,
+              contextType: "LinkUrl",
+              actionUrl: nil,
+              courseId: nil,
+              userId: user_id
+          })
+          HTTParty.post(
+            Rails.configuration.node_site_url + "/api/v1/webhook/sendEmail",
+            body: {
+              to: user.email,
+              subject: "Issue Resolved",
+              message: comment_body.gsub("Resolved:", "").squish,
+              altText: ""
+          })
+        else
+          p "Passed, either format mis-match or issue is resolved!"
+        end       
+      end
+    end
+  end
+end
