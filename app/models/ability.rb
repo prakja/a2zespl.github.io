0957b3906635689d1b2548c31fd1cb8fdcf77373
jay <jay.prakash1@gmail.com>
2020-04-12 13:56:13 +0530

    fix batch resolve for customer issue

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,6 +11,7 @@ class Ability
       can [:read, :update], [Doubt, DoubtAnswer, Question, Video, Test, CustomerIssue, Note]
       can [:read, :create, :update], [VideoAnnotation, VideoLink]
       can [:duplicate_questions, :remove_duplicate, :question_issues], [Topic]
+      can [:batch_action], [CustomerIssue]
     elsif user.role == 'support'
       can :read, ActiveAdmin::Page, :name => "Dashboard"
       can :manage, [SubTopic, Post, ScheduleItem, Delivery, CustomerSupport, Group, Message]
