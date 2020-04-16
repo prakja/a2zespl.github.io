bf00a04b3f4f5dc084503345c49592749e0fd206
jay <jay.prakash1@gmail.com>
2020-04-16 19:37:24 +0530

    test pdf checking page to show question without option prominently

--- app/admin/tests.rb
+++ app/admin/tests.rb
@@ -35,9 +35,9 @@ index do
   column :expiryAt
   column ("Current Question Count") {|test| raw("<b>" + test.questions.count.to_s + "</b>") + "/" + raw(test.numQuestions)}
   # column ("All Questions") {|test| link_to 'All Test Questions', "../../admin/questions?order=sequenceId_asc_and_id_asc&showProofRead=yes&q[questionTests_testId_eq]=" + test.id.to_s}
-  column ("Add/Delete Questions") {|test| raw('<a target="_blank" href=/tests/crud_question/' + test.id.to_s + '>Add/Delete Questions</a>')}
-  column ("Get PDF") {|test| raw('<a target="_blank" href=https://www.neetprep.com/test-question/' + test.id.to_s + '?white&showId=true&orderBy=SEQASC>Get PDF</a>')}
-  column ("Get PDF with Solution") {|test| raw('<a target="_blank" href=https://admin1.neetprep.com/questions/test_question_pdf/' + test.id.to_s + '>Get PDF with Solution</a>')}
+  column ("Add/Delete Questions") {|test| raw('<a target="_blank" href="/tests/crud_question/' + test.id.to_s + '">Add/Delete Questions</a>')}
+  column ("Get PDF") {|test| raw('<a target="_blank" href="https://www.neetprep.com/test-question/' + test.id.to_s + '?white&showId=true&orderBy=SEQASC">Get PDF</a>')}
+  column ("Get PDF with Solution") {|test| raw('<a target="_blank" href="/questions/test_question_pdf/' + test.id.to_s + '">Get PDF with Solution</a>')}
   column ("History") {|test| raw('<a target="_blank" href="/admin/tests/' + (test.id).to_s + '/history">View History</a>')}
   actions
 end
