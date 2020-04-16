b5364b50ac033a3cf760afbdfd05edd598ac02f5
shivamgooded <shivam@goodeducator.com>
2020-04-16 13:31:12 +0530

    video import fix

--- app/admin/videos.rb
+++ app/admin/videos.rb
@@ -13,6 +13,7 @@ ActiveAdmin.register Video do
 #   permitted
 # end
   active_admin_import validate: true,
+    batch_size: 1,
     timestamps: true,
     headers_rewrites: { 'name': :name, 'description': :description, 'thumbnail': :thumbnail, 'url': :url, 'language': :language, 'createdAt': :createdAt, 'updatedAt': :updatedAt },
     before_batch_import: ->(importer) {
