e1512806380b7536f80358754e3d6453e15215a7
jay <jay.prakash1@gmail.com>
2020-04-18 16:07:17 +0530

    physics issues

--- app/models/customer_issue.rb
+++ app/models/customer_issue.rb
@@ -15,10 +15,10 @@ class CustomerIssue < ApplicationRecord
   where(resolved: false)
  }
 
- scope :botany_issues, -> {subject_name([53, 478, 132, 495, 390, 222]).non_resolved().distinct}
- scope :chemistry_issues, -> {subject_name([54, 477, 129, 494, 391, 229, 169]).non_resolved().distinct}
- scope :physics_issues, -> {subject_name([55, 476, 126, 493, 392, 232, 170]).non_resolved().distinct}
- scope :zoology_issues, -> {subject_name([56, 479, 135, 496, 393, 234]).non_resolved().distinct}
+ scope :botany_issues, -> {subject_name([53, 478, 132, 495, 390, 222]).non_resolved()}
+ scope :chemistry_issues, -> {subject_name([54, 477, 129, 494, 391, 229, 169]).non_resolved()}
+ scope :physics_issues, -> {subject_name([55, 476, 126, 493, 392, 232, 170]).non_resolved()}
+ scope :zoology_issues, -> {subject_name([56, 479, 135, 496, 393, 234]).non_resolved()}
 
  def self.ransackable_scopes(_auth_object = nil)
   [:subject_name]
