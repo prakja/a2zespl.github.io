0a528d7699c1b0840e935d5d24c5444108856299
jay <jay.prakash1@gmail.com>
2020-04-22 14:54:28 +0530

    import SubjectChapter entries -> create or update

--- app/admin/subject_chapters.rb
+++ app/admin/subject_chapters.rb
@@ -5,24 +5,16 @@ ActiveAdmin.register SubjectChapter do
   active_admin_import validate: true,
     timestamps: false,
     batch_size: 1,
-    headers_rewrites: { 'chapterId': :chapterId, 'subjectId': :subjectId, 'createdAt': :createdAt, 'updatedAt': :updatedAt },
+    on_duplicate_key_update: [:subjectId, :chapterId],
+    headers_rewrites: { 'id': :id, 'chapterId': :chapterId, 'subjectId': :subjectId},
     before_batch_import: lambda { |importer|
-                           # add created at and upated at
-                           time = Time.zone.now
-                           importer.csv_lines.each do |line|
-                             p line
-                             importer.options['time'] = time
-                             line.insert(-1, time)
-                             line.insert(-1, time)
-                           end
+                          p "before_import"
                          },
     after_batch_import: lambda { |importer|
                           p "after_import"
                         },
     template_object: ActiveAdminImport::Model.new(
-        hint: "File will be imported with such header format: chapterId', 'subjectId'.
-        Remove the header from the CSV before uploading.",
-        csv_headers: ['chapterId',	'subjectId', 'createdAt', 'updatedAt']
+        hint: "File will be imported with such header format: 'chapterId', 'subjectId', 'id' (optional) in any order."
     )
 
   index do
