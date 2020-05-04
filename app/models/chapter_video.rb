3cbff6cacd4122fa1612534652d6a1f2ba3cf51c
jay <jay.prakash1@gmail.com>
2020-05-04 18:52:10 +0530

    import chapter videos

--- app/models/chapter_video.rb
+++ app/models/chapter_video.rb
@@ -1,5 +1,17 @@
 class ChapterVideo < ApplicationRecord
   self.table_name = "ChapterVideo"
+
+  before_create :setCreatedTime, :setUpdatedTime
+  before_update :setUpdatedTime
+
+  def setCreatedTime
+    self.createdAt = Time.now
+  end
+
+  def setUpdatedTime
+    self.updatedAt = Time.now
+  end
+
   belongs_to :video, foreign_key: 'videoId', optional: true
   belongs_to :topic, foreign_key: 'chapterId', class_name: 'Topic', optional: true
   attribute :createdAt, :datetime, default: Time.now
