9d6fbca01bdb2960bc592415a2c26d2ccc89c602
jay <jay.prakash1@gmail.com>
2020-05-13 16:42:00 +0530

    adding chapter questions for import

---
+++ app/admin/chapter_questions.rb
@@ -0,0 +1,42 @@
+ActiveAdmin.register ChapterQuestion do 
+  remove_filter :topic, :question
+
+  active_admin_import validate: true,
+    timestamps: false,
+    batch_size: 1,
+    on_duplicate_key_update: [:questionId, :chapterId],
+    headers_rewrites: { 'id': :id, 'chapterId': :chapterId, 'questionId': :questionId},
+    before_batch_import: lambda { |importer|
+                          p "before_import"
+                         },
+    after_batch_import: lambda { |importer|
+                          p "after_import"
+                        },
+    template_object: ActiveAdminImport::Model.new(
+        hint: "File will be imported with such header format: 'chapterId', 'questionId', 'id' (optional) in any order."
+    )
+
+  index do
+    selectable_column
+    id_column
+    column ("Question") { |cv|
+      auto_link(cv.question)
+    }
+    column ("Chapter") {|cv|
+      auto_link(cv.topic)
+    }
+  end
+
+  controller do
+    def scoped_collection
+      super.includes(:question, :topic)
+    end
+  end
+
+  csv do
+    column :id
+    column :questionId
+    column :chapterId
+  end
+
+end
--- app/models/chapter_question.rb
+++ app/models/chapter_question.rb
@@ -1,5 +1,17 @@
 class ChapterQuestion < ApplicationRecord
   self.table_name = "ChapterQuestion"
+
+  before_create :setCreatedTime, :setUpdatedTime
+  before_update :setUpdatedTime
+
+  def setCreatedTime
+    self.createdAt = Time.now
+  end
+
+  def setUpdatedTime
+    self.updatedAt = Time.now
+  end
+
   belongs_to :question, foreign_key: 'questionId', optional: true
   belongs_to :topic, foreign_key: 'chapterId', class_name: 'Topic', optional: true
   attribute :createdAt, :datetime, default: Time.now
