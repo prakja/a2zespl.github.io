193378ab71523ee4e5905ce49c32ec8b1d97b6a3
jay <jay.prakash1@gmail.com>
2020-06-03 19:33:15 +0530

    test wise table columns

--- app/controllers/test_attempts_controller.rb
+++ app/controllers/test_attempts_controller.rb
@@ -10,12 +10,20 @@ class TestAttemptsController < ApplicationController
     end
 
     @test_attempts = {}
-    @test_attempts = TestAttempt.aryan_raj_test_series.score_gte(600).includes(:test, user: :user_profile).order("\"TestAttempt\".\"testId\" desc, \"result\"->>'totalMarks' desc").pluck("\"TestAttempt\".\"userId\", \"UserProfile\".\"displayName\" , \"Test\".\"name\", \"result\"->>'totalMarks'")
+    @tests = TestAttempt.aryan_raj_test_series.order('"Test"."id" ASC').pluck('distinct "Test"."id", "Test"."name"');
+    @test_attempts = TestAttempt.aryan_raj_test_series.score_gte(600).includes(:test, user: :user_profile).order("\"TestAttempt\".\"testId\" desc, \"result\"->>'totalMarks' desc").pluck("\"TestAttempt\".\"userId\", \"UserProfile\".\"displayName\", \"Test\".\"id\", \"result\"->>'totalMarks', COALESCE(\"UserProfile\".\"phone\", \"UserProfile\".\"phone\")")
 
     @report_data = {}
 
     @test_attempts.each do |attempt|
-      @report_data[attempt[0].to_s] = [attempt[1].to_s, attempt[2].to_s, attempt[3].to_s]
+      if @report_data[attempt[0].to_s].nil?
+        @report_data[attempt[0].to_s] = {}
+        @report_data[attempt[0].to_s]["name"] = attempt[1].to_s
+        @report_data[attempt[0].to_s]["phone"] = attempt[4].to_s
+        @report_data[attempt[0].to_s][attempt[2].to_s] = attempt[3].to_s
+      else
+        @report_data[attempt[0].to_s][attempt[2].to_s] = attempt[3].to_s
+      end
     end
 
   end
