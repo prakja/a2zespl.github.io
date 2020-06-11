389953f7883d847e3d0605526930311ba710cc50
jay <jay.prakash1@gmail.com>
2020-06-11 15:58:47 +0530

    allow video download change from user profile

--- app/admin/user_profiles.rb
+++ app/admin/user_profiles.rb
@@ -1,23 +1,20 @@
 ActiveAdmin.register UserProfile do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
-remove_filter :user
-
-controller do
-  def scoped_collection
-    super.includes(user: :user_profile)
+  remove_filter :user
+  permit_params :displayName, :allowVideoDownload, :picture
+  controller do
+    def scoped_collection
+      super.includes(user: :user_profile)
+    end
   end
-end
+
+  form do |f|
+    f.inputs "UserProfile" do
+      f.input :displayName
+      f.input :allowVideoDownload
+      f.input :picture
+    end
+    f.actions
+  end
 
 end
