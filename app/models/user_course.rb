f693efdad0c3cfbc897354961ae83aecdee0328c
jay prakash <jay.prakash1@gmail.com>
2020-08-19 15:21:11 +0530

    show active user courses by default

--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -1,10 +1,14 @@
 class UserCourse < ApplicationRecord
- self.table_name = "UserCourse"
- has_paper_trail
- belongs_to :course, foreign_key: "courseId", class_name: "Course", optional: false
- belongs_to :invitation, foreign_key: "invitationId", class_name: "CourseInvitation", optional: true
- belongs_to :user, foreign_key: "userId", class_name: "User"
- # belongs_to :courseInvitation, foreign_key: "invitationId", class_name: "CourseInvitation", optional: true
- attribute :createdAt, :datetime, default: Time.now
- attribute :updatedAt, :datetime, default: Time.now
+  self.table_name = "UserCourse"
+  has_paper_trail
+  belongs_to :course, foreign_key: "courseId", class_name: "Course", optional: false
+  belongs_to :invitation, foreign_key: "invitationId", class_name: "CourseInvitation", optional: true
+  belongs_to :user, foreign_key: "userId", class_name: "User"
+  # belongs_to :courseInvitation, foreign_key: "invitationId", class_name: "CourseInvitation", optional: true
+  attribute :createdAt, :datetime, default: Time.now
+  attribute :updatedAt, :datetime, default: Time.now
+
+  scope :active, ->() {
+    where('"UserCourse"."startedAt" <= current_timestamp and "UserCourse"."expiryAt" > current_timestamp')
+  }
 end
