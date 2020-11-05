afcacbc264bbc2a94f3a19a4cfb7874f67e59caa
jay prakash <jay.prakash1@gmail.com>
2020-11-05 16:05:05 +0530

    fix syntax error in last commit

--- app/models/test_leader_board.rb
+++ app/models/test_leader_board.rb
@@ -5,6 +5,6 @@ class TestLeaderBoard < ApplicationRecord
   belongs_to :user, class_name: "User", foreign_key: "userId"
   belongs_to :test_attempt, class_name: "TestAttempt", foreign_key: "testAttemptId"
   scope :paid_students, -> {where(UserCourse.where('"UserCourse"."userId" = "TestLeaderBoard"."userId"').limit(1).arel.exists)}
-  scope :high_yield_paid_students, -> {where(UserCourse.where('"UserCourse"."userId" = "TestLeaderBoard"."userId" and "UserCourse"."courseId" in (' + Rails.config.aryan_raj_test_series_1_yr.to_s + ')').limit(1).arel.exists)}
+  scope :high_yield_paid_students, -> {where(UserCourse.where('"UserCourse"."userId" = "TestLeaderBoard"."userId" and "UserCourse"."courseId" in (' + Rails.configuration.aryan_raj_test_series_1_yr.to_s + ')').limit(1).arel.exists)}
 
 end
