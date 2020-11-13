c794bb3fec89ad99fcaf6cfb3359b02b2d90240f
jay prakash <jay.prakash1@gmail.com>
2020-11-13 11:48:07 +0530

    allow feeDesc to be updated

--- app/admin/courses.rb
+++ app/admin/courses.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Course do
-  permit_params :name, :year, :image, :description, :package, :fee, :public, :hasVideo, :hasQuestionBank, :hasNCERT, :hasDoubt, :hasLeaderBoard, :allowCallback, :origFee, :discount, :type, :bestSeller, :recommended, :discountedFee, :expiryAt, :hasPartTest, :shortDescription, :seqId
+  permit_params :name, :year, :image, :description, :package, :fee, :public, :hasVideo, :hasQuestionBank, :hasNCERT, :hasDoubt, :hasLeaderBoard, :allowCallback, :origFee, :discount, :type, :bestSeller, :recommended, :discountedFee, :expiryAt, :hasPartTest, :shortDescription, :seqId, :feeDesc
   remove_filter :payments, :subjects, :versions, :courseInvitations, :courseCourseTests, :tests, :public_courses, :course_offers
 
   sidebar :related_data, only: :show do
