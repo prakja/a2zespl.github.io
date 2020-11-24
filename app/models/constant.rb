479ec237cf0f725c9067bddb2c9b8a6684b57d7e
Sanchit Samuel <sanchit.samuel@live.com>
2020-11-24 13:36:52 +0530

    *constants changes

--- app/models/constant.rb
+++ app/models/constant.rb
@@ -1,3 +1,14 @@
 class Constant < ApplicationRecord
   self.table_name = "Constant"
+
+  before_create :set_value
+  before_update :set_value
+
+  def set_value
+    if self.value.blank?
+      self.value = nil
+    else
+      self.value = JSON.parse(self.value)
+    end
+  end
 end
