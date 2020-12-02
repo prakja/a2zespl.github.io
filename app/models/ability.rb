94ec436ea88d7b2c2963595291b5057fcb6908e0
jay prakash <jay.prakash1@gmail.com>
2020-12-02 18:05:45 +0530

    allow most roles to edit user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,31 +10,31 @@ class Ability
     elsif user.role == 'faculty'
       can :read, ActiveAdmin::Page, :name => "Dashboard"
       can :manage, [SubTopic, QuestionHint, ChapterFlashCard, FlashCard]
-      can :read, [UserProfile, User, Notification, SubjectLeaderBoard, TopicLeaderBoard, CommonLeaderBoard, TestLeaderBoard, Answer, CourseTest, Topic, CustomerIssueType, UniqueDoubtAnswer]
-      can [:read, :create, :update], [Doubt, DoubtAnswer, Question, Video, Test, CustomerIssue, Note, QuestionTranslation]
-      can [:read, :create, :update], [VideoAnnotation, VideoLink]
+      can :read, [UserProfile, Notification, SubjectLeaderBoard, TopicLeaderBoard, CommonLeaderBoard, TestLeaderBoard, Answer, CourseTest, Topic, CustomerIssueType, UniqueDoubtAnswer]
+      can [:read, :update], [Doubt, User, CustomerIssue, QuestionTranslation]
+      can [:read, :create, :update], [VideoAnnotation, VideoLink, DoubtAnswer, Question, Video, Test, Note]
       can [:duplicate_questions, :remove_duplicate, :question_issues], [Topic]
       can [:batch_action], [CustomerIssue]
       can [:create, :read], [ActiveAdmin::Comment]
     elsif user.role == 'superfaculty'
       can :read, ActiveAdmin::Page, :name => "Dashboard"
       can :manage, [SubTopic, QuestionHint, ChapterFlashCard, FlashCard]
-      can :read, [UserProfile, User, Notification, SubjectLeaderBoard, TopicLeaderBoard, CommonLeaderBoard, TestLeaderBoard, Answer, CourseTest, Topic, CustomerIssueType, UniqueDoubtAnswer]
-      can [:read, :update], [Doubt, DoubtAnswer, Question, Video, Test, CustomerIssue, Note, QuestionTranslation]
-      can [:read, :create, :update], [VideoAnnotation, VideoLink]
+      can :read, [UserProfile, Notification, SubjectLeaderBoard, TopicLeaderBoard, CommonLeaderBoard, TestLeaderBoard, Answer, CourseTest, Topic, CustomerIssueType, UniqueDoubtAnswer]
+      can [:read, :update], [Doubt, User, CustomerIssue, QuestionTranslation]
+      can [:read, :create, :update], [VideoAnnotation, VideoLink, DoubtAnswer, Question, Video, Test, Note]
       can [:duplicate_questions, :remove_duplicate, :question_issues], [Topic]
       can [:batch_action], [CustomerIssue]
       can [:create, :read], [ActiveAdmin::Comment]
     elsif user.role == 'supportAndFaculty'
       can :read, ActiveAdmin::Page, :name => "Dashboard"
       can :manage, [SubTopic, Post, ScheduleItem, Delivery, CustomerSupport, Group, Message, FlashCard, ChapterFlashCard]
-      can :read, [UserCourse, User, UserProfile, CustomerIssueType]
-      can [:read, :update], [CustomerIssue]
+      can :read, [UserCourse, UserProfile, CustomerIssueType]
+      can [:read, :update], [User, CustomerIssue]
       can [:create, :read, :update], [Question, Test, Video, CourseInvitation, Payment, TestLeaderBoard, SubjectChapter, FlashCard, Note]
       can :import, [Video, FlashCard]
       can :manage, [SubTopic, QuestionHint, ChapterFlashCard, FlashCard]
-      can :read, [UserProfile, User, Notification, SubjectLeaderBoard, TopicLeaderBoard, CommonLeaderBoard, TestLeaderBoard, Answer, CourseTest, Topic, CustomerIssueType, UniqueDoubtAnswer]
-      can [:read, :update], [Doubt, DoubtAnswer, Question, Video, Test, CustomerIssue, Note, QuestionTranslation]
+      can :read, [UserProfile, Notification, SubjectLeaderBoard, TopicLeaderBoard, CommonLeaderBoard, TestLeaderBoard, Answer, CourseTest, Topic, CustomerIssueType, UniqueDoubtAnswer]
+      can [:read, :update], [Doubt, DoubtAnswer, Question, Video, Test, CustomerIssue, Note, QuestionTranslation, User]
       can [:read, :create, :update], [VideoAnnotation, VideoLink]
       can [:duplicate_questions, :remove_duplicate, :question_issues], [Topic]
       can [:batch_action], [CustomerIssue]
@@ -42,8 +42,8 @@ class Ability
     elsif user.role == 'support'
       can :read, ActiveAdmin::Page, :name => "Dashboard"
       can :manage, [SubTopic, Post, ScheduleItem, Delivery, CustomerSupport, Group, Message, FlashCard, ChapterFlashCard]
-      can :read, [UserCourse, User, UserProfile, CustomerIssueType]
-      can [:read, :update], [CustomerIssue, QuestionTranslation]
+      can :read, [UserCourse, UserProfile, CustomerIssueType]
+      can [:read, :update], [CustomerIssue, QuestionTranslation, User]
       can [:create, :read, :update], [Question, Test, Video, CourseInvitation, Payment, TestLeaderBoard, SubjectChapter, FlashCard, Note]
       can :import, [Video, FlashCard]
     elsif user.role == 'sales' or user.role == 'sales2'
