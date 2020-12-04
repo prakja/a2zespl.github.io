6ab84ceed1e7203e1eb02381d0680b8bb309a72e
jay prakash <jay.prakash1@gmail.com>
2020-12-04 15:22:10 +0530

    fixes for video filtering by subject and course id

--- app/models/video.rb
+++ app/models/video.rb
@@ -44,20 +44,20 @@ class Video < ApplicationRecord
     end
   end
 
-  scope :subject_name, ->(subject_id) {
+  scope :subject_id, ->(subject_id) {
     joins(:topics => :subject).where(topics: {Subject: {id: subject_id}})
   }
 
-  scope :subject_names, ->(*subject_ids) {
+  scope :subject_ids, ->(*subject_ids) {
     flatten_subject_ids = subject_ids.flatten
     joins(:topics => :subject).where(topics: {Subject: {id: flatten_subject_ids}})
   }
 
-  scope :course_name, -> (course_id) {
+  scope :course_id, -> (course_id) {
     joins(:topics => :subject).where(topics: {Subject: {courseId: course_id}})
   }
 
-  scope :course_names, ->(*course_ids) {
+  scope :course_ids, ->(*course_ids) {
     flatten_course_ids = course_ids.flatten
     joins(:topics => :subject).where(topics: {Subject: {courseId: flatten_course_ids}})
   }
@@ -73,4 +73,8 @@ class Video < ApplicationRecord
   scope :neetprep_course, -> {joins(:topics => :subjects).where(topics: {Subject: {courseId: Rails.configuration.hinglish_full_course_id}}).distinct()}
   scope :maths_course, -> {joins(:topics => :subject).where(topics: {Subject: {courseId: Rails.configuration.hinglish_math_course_id}}).distinct()}
   scope :boost_up_course, -> {joins(:topics => :subject).where(topics: {Subject: {courseId: Rails.configuration.boostup_course_id}}).distinct()}
+
+  def self.ransackable_scopes(_auth_object = nil)
+    [:subject_id, :subject_ids, :course_id, :course_ids]
+  end
 end
