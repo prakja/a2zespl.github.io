b753b4b0e9d736f913eeab71c555b0430e61efdf
jay prakash <jay.prakash1@gmail.com>
2021-02-15 17:42:18 +0530

    changes for duplicate questions in subtopic

--- app/admin/sub_topics.rb
+++ app/admin/sub_topics.rb
@@ -23,6 +23,43 @@ ActiveAdmin.register SubTopic do
   scope :zoology, show_count: false
   scope :all, show_count: false
 
+  member_action :duplicate_questions do
+    @subTopic = SubTopic.find(resource.id)
+    cutoff = params[:cutoff].to_f > 0 ? params[:cutoff] : "0.7"
+    @question_pairs = ActiveRecord::Base.connection.query('Select "Question"."id", "Question"."question", "Question1"."id", "Question1"."question", "Question"."correctOptionIndex", "Question1"."correctOptionIndex", "Question"."options", "Question1".options, "Question"."explanation", "Question1"."explanation" from "QuestionSubTopic" INNER JOIN "Question" ON "Question"."id" = "QuestionSubTopic"."questionId" and "Question"."deleted" = false and "Question"."type" = \'MCQ-SO\' and "QuestionSubTopic"."subTopicId" = ' + resource.id.to_s + ' INNER JOIN "QuestionSubTopic" AS "QuestionSubTopic1" ON "QuestionSubTopic1"."subTopicId" = "QuestionSubTopic"."subTopicId" and "QuestionSubTopic"."questionId" < "QuestionSubTopic1"."questionId" INNER JOIN "Question" AS "Question1" ON "Question1"."id" = "QuestionSubTopic1"."questionId" and "Question1"."deleted" = false and "Question1"."type" = \'MCQ-SO\' and not exists (select * from "NotDuplicateQuestion" where "questionId1" = "Question"."id" and "questionId2" = "Question1"."id") and not exists (select * from "DuplicateQuestion" where "questionId1" = "Question"."id" and "questionId2" = "Question1"."id") and similarity("Question1"."question", "Question"."question") > ' + cutoff + ' and "QuestionSubTopic1"."subTopicId" = ' + resource.id.to_s);
+  end
+
+  member_action :mark_not_duplicate, method: :post do
+    NotDuplicateQuestion.create!(
+      questionId1: params[:question_id1].to_i,
+      questionId2: params[:question_id2].to_i
+    )
+    respond_to do |format|
+      format.html {redirect_back fallback_location: duplicate_questions_admin_sub_topic_path(resource), notice: "Marked questions as not duplicate!"}
+      format.js
+    end
+  end
+
+  # remove one of the duplicate question
+  member_action :mark_duplicate, method: :post do
+    begin
+      DuplicateQuestion.create!(
+        questionId1: params[:question_id1].to_i,
+        questionId2: params[:question_id2].to_i
+      )
+    rescue ActiveRecord::RecordNotUnique => e
+      if(e.message =~ /unique.*constraint.*DuplicateQuestion_questionId1_questionId2_key/)
+      else
+        raise e.message
+      end
+    end
+    respond_to do |format|
+      format.html {redirect_back fallback_location: duplicate_questions_admin_sub_topic_path(resource), notice: "Duplicate question marked from topic questions!"}
+      format.js
+    end
+  end
+
+
   index do
     id_column
     column :topic
@@ -32,6 +69,9 @@ ActiveAdmin.register SubTopic do
     end
     if (current_admin_user.role == 'admin' or current_admin_user.role == 'faculty') and (params["scope"].present? or (params[:q].present? and (params[:q][:topic_id_eq].present? or params[:q][:topic_id_in].present?)))
       column :questions_count, sortable: true
+      column ("Duplicate Questions") { |subtopic|
+        link_to "Duplicate Questions", duplicate_questions_admin_sub_topic_path(id: subtopic.id)
+      }
     end
     actions
   end
