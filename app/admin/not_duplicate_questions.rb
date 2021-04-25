241b8dbc589c1097af9302e90aceb825abd8f321
Jay Prakash <jprakash@goodeducator.com>
2021-04-25 23:33:29 +0530

    not duplicate question scope and linking to possible contradicting duplicate

--- app/admin/not_duplicate_questions.rb
+++ app/admin/not_duplicate_questions.rb
@@ -1,7 +1,9 @@
 ActiveAdmin.register NotDuplicateQuestion do
   remove_filter :question1, :question2, :versions
 
+  scope :marked_duplicate, show_count: false
   index do
+    render partial: 'mathjax'
     if current_admin_user.admin?
       selectable_column
     end
@@ -13,6 +15,27 @@ ActiveAdmin.register NotDuplicateQuestion do
       raw('<a href=' + edit_admin_question_path(ndq.question2) + " target='_blank'>#{ndq.question2.id}</a><br />" + ndq.question2.question)
     end
     actions
+  end
+
+  show do
+    render partial: 'mathjax'
+    attributes_table do
+      row :id
+      row  "Question1" do |ndq|
+        raw('<a href=' + edit_admin_question_path(ndq.question1) + " target='_blank'>#{ndq.question1.id}</a><br />" + ndq.question1.question)
+      end
+      row "Question2" do |ndq|
+        raw('<a href=' + edit_admin_question_path(ndq.question2) + " target='_blank'>#{ndq.question2.id}</a><br />" + ndq.question2.question)
+      end
+      row "Contradicting Duplicate Question" do |ndq|
+        if ndq.contradiction
+          link_to "View Duplicate Question", admin_duplicate_question_path(ndq.contradiction), target: "_blank"
+        else
+          "No Contradiction found"
+        end
+      end
+    end
+    active_admin_comments
   end
 
   controller do
--- app/models/not_duplicate_question.rb
+++ app/models/not_duplicate_question.rb
@@ -2,6 +2,14 @@ class NotDuplicateQuestion < ApplicationRecord
   has_paper_trail
   self.table_name = "NotDuplicateQuestion"
 
+  scope :marked_duplicate, -> {
+    joins(', "DuplicateQuestion"').where('"DuplicateQuestion"."questionId1" = "NotDuplicateQuestion"."questionId1" and "DuplicateQuestion"."questionId2" = "NotDuplicateQuestion"."questionId2"').distinct
+  }
+
+  def contradiction
+    DuplicateQuestion.where(questionId1: self.questionId1, questionId2: self.questionId2).first
+  end
+
   belongs_to :question1, foreign_key: 'questionId1', class_name: "Question"
   belongs_to :question2, foreign_key: 'questionId2', class_name: "Question"
 end
