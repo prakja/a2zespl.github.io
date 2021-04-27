78132902155cefa623951896fc01b0131106fe5c
Jay Prakash <jprakash@goodeducator.com>
2021-04-27 23:19:36 +0530

    in place editing for sentence in video sentence

--- app/models/video_sentence.rb
+++ app/models/video_sentence.rb
@@ -4,7 +4,7 @@ class VideoSentence < ApplicationRecord
     self.table_name = "VideoSentence"
     belongs_to :video, class_name: "Video", foreign_key: "videoId"
     belongs_to :chapter, class_name: "Topic", foreign_key: "chapterId"
-    belongs_to :section, class_name: "Section", foreign_key: "sectionId"
+    belongs_to :section, class_name: "Section", foreign_key: "sectionId", optional: true
     # has_and_belongs_to_many :questions, class_name: 'Question', join_table: 'QuestionNcertSentence', foreign_key: :ncertSentenceId, association_foreign_key: :questionId
   
     before_create :setCreatedTime, :setUpdatedTime
