4d9f42fb4f69de72d74509de767aef5bf738e9a3
Jay Prakash <jprakash@goodeducator.com>
2021-05-01 11:41:18 +0530

    paper trail

--- app/models/question_video_sentence.rb
+++ app/models/question_video_sentence.rb
@@ -1,5 +1,6 @@
 class QuestionVideoSentence < ApplicationRecord
     self.table_name = 'QuestionVideoSentence'
+    has_paper_trail
     belongs_to :question, foreign_key: :questionId, class_name: :Question, touch: true
     belongs_to :VideoSentence, foreign_key: :videoSentenceId, class_name: :VideoSentence
     attribute :createdAt, :datetime, default: Time.now
