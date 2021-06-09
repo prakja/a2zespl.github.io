45cc0b4470485cfb88908fcf1483d771b60b76c1
Jay Prakash <jprakash@goodeducator.com>
2021-06-09 12:09:55 +0530

    ignore user ids - fix for typo

--- app/models/doubt.rb
+++ app/models/doubt.rb
@@ -96,7 +96,7 @@ class Doubt < ApplicationRecord
   scope :botany_paid_student_doubts, -> {ignore_old_doubt("yes").ignored_user_ids.solved('no').paid([8, 141, 20, 100, 51, 120, 518], 'yes').deleted('no').subject_name([53, 478, 132, 495, 390, 222, 447, 983, 990, 1152]).distinct}
   scope :chemistry_paid_student_doubts, -> {ignore_old_doubt("yes").ignored_user_ids.solved('no').paid([8, 141, 19, 100, 51, 518], 'yes').deleted('no').subject_name([54, 477, 129, 494, 391, 229, 169, 984, 987, 991, 1153]).distinct}
   scope :physics_paid_student_doubts, -> {ignore_old_doubt("yes").ignored_user_ids.solved('no').paid([8, 141, 18, 100, 51, 518], 'yes').deleted('no').subject_name([55, 476, 126, 493, 392, 232, 170, 985, 988, 992, 1154]).distinct}
-  scope :zoology_paid_student_doubts, -> {ignore_old_doubt("yes").ignore_user_ids.solved('no').paid([8, 141, 20, 100, 51, 120, 518], 'yes').deleted('no').subject_name([56, 479, 135, 496, 393, 234, 448, 986, 989, 1155]).distinct}
+  scope :zoology_paid_student_doubts, -> {ignore_old_doubt("yes").ignored_user_ids.solved('no').paid([8, 141, 20, 100, 51, 120, 518], 'yes').deleted('no').subject_name([56, 479, 135, 496, 393, 234, 448, 986, 989, 1155]).distinct}
   scope :masterclass_paid_student_doubts, -> {ignore_old_doubt("yes").solved('no').paid([253, 254, 255], 'yes').deleted('no').subject_name([627, 628, 629, 630]).distinct}
   scope :all_masterclass_paid_student_doubts, -> {paid([253, 254, 255], 'yes').deleted('no').subject_name([627, 628, 629, 630]).distinct}
   scope :concept_building_student_doubts, -> {ignore_old_doubt("yes").solved('no').paid([617], 'yes').deleted('no').subject_name([1049]).distinct}
