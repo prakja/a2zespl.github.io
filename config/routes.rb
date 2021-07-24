96892609bb9cd062af5b7dfa8756dfe72c85935c
Jay Prakash <jprakash@goodeducator.com>
2021-07-24 16:55:50 +0530

    change to domain to make auto redirect work

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Permanently redirect all requests originally to admin1.neetprep.com to newadmin.neetprep.com
   get '/(*path)', to: redirect { |path_params, request|
     "https://newadmin.neetprep.com/#{path_params[:path]}"
-  }, status: 301, constraints: { domain: 'admin1.neetprep.com' }
+  }, status: 301, constraints: { domain: 'https://admin1.neetprep.com' }
 
   # ... Remaining application routes
   devise_for :admin_users, ActiveAdmin::Devise.config
