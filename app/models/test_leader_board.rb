3d5540b67f6da56d61d0327eaf067e7c32c8919e
Jay Prakash <jprakash@goodeducator.com>
2021-09-24 11:48:12 +0530

    fix for paid student logic in test leader board. no trial course access is not included

--- app/models/test_leader_board.rb
+++ app/models/test_leader_board.rb
@@ -4,8 +4,8 @@ class TestLeaderBoard < ApplicationRecord
   belongs_to :test, class_name: "Test", foreign_key: "testId"
   belongs_to :user, class_name: "User", foreign_key: "userId"
   belongs_to :test_attempt, class_name: "TestAttempt", foreign_key: "testAttemptId"
-  scope :paid_students, -> {where(UserCourse.where('"UserCourse"."userId" = "TestLeaderBoard"."userId"').limit(1).arel.exists)}
-  scope :high_yield_paid_students, -> {where(UserCourse.where('"UserCourse"."userId" = "TestLeaderBoard"."userId" and "UserCourse"."courseId" in (' + Rails.configuration.aryan_raj_test_series_1_yr.to_s + ')').limit(1).arel.exists)}
-  scope :inspire_students, -> {where(UserCourse.where('"UserCourse"."userId" = "TestLeaderBoard"."userId" and "UserCourse"."courseId" in (' + Rails.configuration.aryan_raj_test_series_2_yr.to_s + ')').limit(1).arel.exists)}
+  scope :paid_students, -> {where(UserCourse.where('"UserCourse"."userId" = "TestLeaderBoard"."userId" and "expiryAt" > "createdAt" + interval \'10 days\'').limit(1).arel.exists)}
+  scope :high_yield_paid_students, -> {where(UserCourse.where('"UserCourse"."userId" = "TestLeaderBoard"."userId" and "UserCourse"."courseId" in (' + Rails.configuration.aryan_raj_test_series_1_yr.to_s + ') and "expiryAt" > "createdAt" + interval \'10 days\'').limit(1).arel.exists)}
+  scope :inspire_students, -> {where(UserCourse.where('"UserCourse"."userId" = "TestLeaderBoard"."userId" and "UserCourse"."courseId" in (' + Rails.configuration.aryan_raj_test_series_2_yr.to_s + ') and "expiryAt" > "createdAt" + interval \'10 days\'').limit(1).arel.exists)}
 
 end
