bd673e3835af485d2d7840089ffbeedf3a80ed25
Jay Prakash <jprakash@goodeducator.com>
2021-12-24 17:48:12 +0530

    don't add extra p elements

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -19,6 +19,9 @@ CKEDITOR.editorConfig = function( config )
   config.autoGrow_bottomSpace = 50;
   config.allowedContent = true;
   config.contentsCss = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
+  config.enterMode = CKEDITOR.ENTER_BR;
+  config.autoParagraph = false;
+  config.fillEmptyBlocks = false;
 
   // Define changes to default configuration here. For example:
   // config.language = 'fr';
