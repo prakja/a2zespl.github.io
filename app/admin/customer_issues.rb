164f0ca7492fe73799d25c977dfe0387e17860c9
Jay Prakash <jprakash@goodeducator.com>
2022-01-14 17:27:42 +0530

    allow configurable issue count on customer issues

--- app/admin/customer_issues.rb
+++ app/admin/customer_issues.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register CustomerIssue do
+  config.per_page = [15, 30, 50] 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
